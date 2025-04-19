e7afe6cf7b5f5b7f6459774cbdaca3d6297434bc
Rahul <rahulmishra4317@gmail.com>
2025-04-19 12:21:25 +0530

    Database Setup

--- backend/db/database.py
+++ backend/db/database.py
@@ -1,17 +1,13 @@
 import os 
 from dotenv import load_dotenv
 load_dotenv()
-
-#database imports 
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.declarative import declarative_base 
 from sqlalchemy.orm import sessionmaker
 
 postgres_connection_string = os.getenv("POSTGRESQL_CONNECTION_STRING")
-
 engine = create_engine("postgres_connection_string")
-
-SessionLocal = sessionmaker(autoflush=False , autocommit = False , bind = engine)
+SessionLocal = sessionmaker(autoflush=False , autocommit = False , bind=engine)
 
 Base = declarative_base()
 
@@ -19,5 +15,5 @@ def get_db():
     db = SessionLocal()
     try :
         yield db
-    finally:
+    finally : 
         db.close()
