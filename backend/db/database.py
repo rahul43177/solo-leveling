d69dd2ffc4cd93daa8b1a7c51cbaf83a10be1888
Rahul <rahulmishra4317@gmail.com>
2025-04-19 22:59:45 +0530

    Database setup self

--- backend/db/database.py
+++ backend/db/database.py
@@ -8,15 +8,16 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 postgres_connection_string = os.getenv("POSTGRESQL_CONNECTION_STRING")
+if not postgres_connection_string:
+    raise ValueError("POSTGRESQL_CONNECTION_STRING environment variable not set")
+
 engine = create_engine(postgres_connection_string)
-SessionLocal = sessionmaker(autoflush=False , autocommit = False , bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 def get_db():
     db = SessionLocal()
-    try :
-        yield db 
-    finally : 
+    try:
+        yield db
+    finally:
         db.close()
-
-
