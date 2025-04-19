0e08f3d6d735eb2cce322cabe51e874048c9fd34
Rahul <rahulmishra4317@gmail.com>
2025-04-20 00:53:16 +0530

    Database connection

--- backend/db/database.py
+++ backend/db/database.py
@@ -1,23 +1,18 @@
 import os 
 from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base , sessionmaker
 
 load_dotenv()
 
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-
 postgres_connection_string = os.getenv("POSTGRESQL_CONNECTION_STRING")
-if not postgres_connection_string:
-    raise ValueError("POSTGRESQL_CONNECTION_STRING environment variable not set")
-
 engine = create_engine(postgres_connection_string)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autcommit = False , autoflush = False , bind = engine)
 Base = declarative_base()
 
 def get_db():
     db = SessionLocal()
-    try:
+    try :
         yield db
     finally:
         db.close()
