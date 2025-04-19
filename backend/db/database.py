f95a300d92c8ab97418dbc3681c5ea36dfb20e9e
Rahul <rahulmishra4317@gmail.com>
2025-04-19 18:39:02 +0530

    Database setup + Supabase addition

--- backend/db/database.py
+++ backend/db/database.py
@@ -1,19 +1,22 @@
 import os 
 from dotenv import load_dotenv
+
 load_dotenv()
+
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base 
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 postgres_connection_string = os.getenv("POSTGRESQL_CONNECTION_STRING")
-engine = create_engine("postgres_connection_string")
+engine = create_engine(postgres_connection_string)
 SessionLocal = sessionmaker(autoflush=False , autocommit = False , bind=engine)
-
 Base = declarative_base()
 
 def get_db():
     db = SessionLocal()
     try :
-        yield db
+        yield db 
     finally : 
         db.close()
+
+
