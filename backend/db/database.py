549fc3b74e19501e8ed9b27caf5b3bc483c2907d
Rahul <rahulmishra4317@gmail.com>
2025-04-26 21:44:48 +0530

    changed the location of requirement file and changed the database code

--- backend/db/database.py
+++ backend/db/database.py
@@ -1,2 +1,23 @@
 import os 
-from sqlalchemy 
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker , declarative_base
+
+load_dotenv()
+
+postgres_connection_string = os.getenv("POSTGRESQL_CONNECTION_STRING")
+
+engine = create_engine(postgres_connection_string)
+
+SessionLocal = sessionmaker(autocommit = False , autoflush= False , bind = engine)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try :
+        yield db 
+    finally:
+        db.close()
+
+
